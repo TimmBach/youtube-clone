1944416645da1798fe4f81f5e2de83a7b3396ccd
TimmBach <follow.timothy@gmail.com>
2021-03-30 00:27:39 +0100

    initialized youtube clone app

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyD-C4QO8E74os-yDZZYeOvi7c2r4lz-JA4",
+  authDomain: "clone-7efbd.firebaseapp.com",
+  projectId: "clone-7efbd",
+  storageBucket: "clone-7efbd.appspot.com",
+  messagingSenderId: "500516174222",
+  appId: "1:500516174222:web:8260a418d1c5876f1c8cdf",
+  measurementId: "G-0Q90QJ414M",
+};
