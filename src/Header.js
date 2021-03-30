40a492b5552f70eb8775380b8d8f8b2f6300983b
TimmBach <follow.timothy@gmail.com>
2021-03-30 03:41:49 +0100

    finished styling the header

---
+++ src/Header.js
@@ -0,0 +1,50 @@
+import React from "react";
+import MenuIcon from "@material-ui/icons/Menu";
+import SearchIcon from "@material-ui/icons/Search";
+import VideoCallIcon from "@material-ui/icons/VideoCall";
+import AppsIcon from "@material-ui/icons/Apps";
+import NotificationsIcon from "@material-ui/icons/Notifications";
+import MicIcon from "@material-ui/icons/Mic";
+import { Avatar } from "@material-ui/core";
+import "./Header.css";
+
+function Header() {
+  return (
+    <>
+      <h1>Timothy's youtube-clone! </h1>
+      <div className="header">
+        <div className="header__left">
+          <MenuIcon />
+          <img
+            className="header__logo"
+            src="https://www.freeiconspng.com/uploads/youtube-logo-png-transparent-image-5.png"
+            alt=""
+          />
+        </div>
+
+        <div className="header__input">
+          <div className="header__inputText">
+            <input placeholder="Search" type="text" />
+            <SearchIcon className="header__inputButton" />
+          </div>
+          <div className="header__inputVoice">
+            <MicIcon />
+          </div>
+        </div>
+
+        <div className="header__right">
+          <VideoCallIcon className="header__icon" />
+          <AppsIcon className="header__icon" />
+          <NotificationsIcon className="header__icon" />
+          <Avatar
+            className="header__icon"
+            alt="Tim"
+            src="https://e7.pngegg.com/pngimages/194/608/png-clipart-cristiano-ronaldo-holding-soccer-ball-cristiano-ronaldo-fifa-18-fifa-17-fifa-16-real-madrid-c-f-fifa-tshirt-blue-thumbnail.png"
+          />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Header;
---
+++ src/RecommendedVideos.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function RecommendedVideos() {
+  return <div></div>;
+}
+
+export default RecommendedVideos;
